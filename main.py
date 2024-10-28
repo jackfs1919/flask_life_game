1bf0943abadb0d1aaa82927adac71228ec915a89
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2024-10-28 17:09:22 +0300

    +

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import webbrowser
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    # webbrowser.open_new_tab("http://127.0.0.1:5000")
+    app.run(debug=True)
