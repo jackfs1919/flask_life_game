ee7dc1f618fecc6296b4afa09e5c028f6a95e6e6
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2024-10-28 17:42:46 +0300

    7

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/live")
+def live():
+    return render_template("live.html")
+
+
 if __name__ == "__main__":
-    # webbrowser.open_new_tab("http://127.0.0.1:5000")
+    webbrowser.open_new_tab("http://127.0.0.1:5000")
     app.run(debug=True)
