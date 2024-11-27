e4eb5e459143db8eb0e1265e824b1227624d41fa
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2024-11-27 14:35:36 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,1 +1,9 @@
 ##%%
+from itertools import permutations
+from pprint import pprint as print
+x = ('исправить', 'создать', 'указать', 'добавить nk', 'добавить')
+y = ('init', 'CMake', 'secur', 'einit', 'например')
+print(list(permutations(y)))
+
+
+
