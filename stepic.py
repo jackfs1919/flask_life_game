17c59f85e92dd2ffbb70aafe5dfe71cf5f07a01e
Den <New@KOMPUTER.com>
2024-12-05 23:11:49 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,9 +1,8 @@
 ##%%
-from itertools import permutations
-from pprint import pprint as print
-x = ('исправить', 'создать', 'указать', 'добавить nk', 'добавить')
-y = ('init', 'CMake', 'secur', 'einit', 'например')
-print(list(permutations(y)))
-
+# from itertools import permutations
+# from pprint import pprint as print
+# x = ('исправить', 'создать', 'указать', 'добавить nk', 'добавить')
+# y = ('init', 'CMake', 'secur', 'einit', 'например')
+# print(list(permutations(y)))
 
 
