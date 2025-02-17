eeaf90b71a51f179f0afc77defa53170aba9b331
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2025-02-17 11:10:32 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,15 +1,35 @@
 ##%%
-class StringValidation:
+from time import sleep
+from playwright.sync_api import Playwright, sync_playwright
+URL = "https://parsinger.ru/selenium/3/3.2.4/index.html"
+# M1SS10N-P0SS1BL3
 
-    def __set_name__(self, owner_class, attribute_name):
-        self.attribute_name = attribute_name
 
-    def __set__(self, instance, value):
-        if not isinstance(value, str):
-            raise ValueError(f'В атрибут {self.attribute_name} можно сохранять только строки.')
-        instance.__dict__[self.attribute_name] = value
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    
+    def req_resp():
+        '''
+        вывод на экран результатаов GET и POST запросов к page
+        вызывается после page = context.new_page()
+        '''
+        page.on("request", lambda request: print(">>", request.method, request.url))
+        page.on("response", lambda response: print("<<", response.status, response.url))
 
-    def __get__(self, instance, owner_class):
-        if instance is None:
-            return self
-        return instance.__dict__.get(self.attribute_name, None)
+    req_resp()
+    page.goto(URL)
+    page.locator("#secret-key-button").click()
+    kod = page.locator("#secret-key-button").get_attribute("data")
+#     page.locator("#userInput").type(kod)
+#     page.locator("#checkBtn").click()
+    print(kod)
+    sleep(10)
+    context.close()
+    browser.close()
+    exit()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
