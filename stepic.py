aa882d4ce2c093511e417fd3fee376963c035668
Den <New@KOMPUTER.com>
2025-02-20 20:08:45 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,15 +1,2 @@
 ##%%
-class StringValidation:
-
-    def __set_name__(self, owner_class, attribute_name):
-        self.attribute_name = attribute_name
-
-    def __set__(self, instance, value):
-        if not isinstance(value, str):
-            raise ValueError(f'В атрибут {self.attribute_name} можно сохранять только строки.')
-        instance.__dict__[self.attribute_name] = value
-
-    def __get__(self, instance, owner_class):
-        if instance is None:
-            return self
-        return instance.__dict__.get(self.attribute_name, None)
+print(not (False or False))
