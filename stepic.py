169a89fd6def10d256354d0333c840ec910a66a3
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2025-02-20 18:54:23 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,8 +1,7 @@
 ##%%
 from time import sleep
 from playwright.sync_api import Playwright, sync_playwright
-URL = "https://parsinger.ru/selenium/3/3.2.4/index.html"
-# M1SS10N-P0SS1BL3
+URL = "https://parsinger.ru/selenium/3/3.3.3/index.html"
 
 
 def run(playwright: Playwright) -> None:
@@ -18,15 +17,22 @@ def run(playwright: Playwright) -> None:
         page.on("request", lambda request: print(">>", request.method, request.url))
         page.on("response", lambda response: print("<<", response.status, response.url))
 
-    req_resp()
+    # req_resp()
     page.goto(URL)
-    page.locator("#secret-key-button").click()
-    kod = page.locator("#secret-key-button").get_attribute("data")
-#     page.locator("#userInput").type(kod)
-#     page.locator("#checkBtn").click()
-    print(kod)
+    # page.locator("#secret-key-button").click()
+    par = page.locator("a").all()
+    colvo = 0
+    for i in par:
+        attr = i.get_attribute("stormtrooper")
+        if attr.isdigit():
+            colvo += int(attr)
+    page.locator("#inputNumber").click()
+    page.locator("#inputNumber").type(str(colvo))
+    page.locator("#feedbackMessage").click()
+    # par.locator(".child_class").first.click()
+    print(page.locator("#feedbackMessage").text_content())
     sleep(10)
-    context.close()
+    # context.close()
     browser.close()
     exit()
 
