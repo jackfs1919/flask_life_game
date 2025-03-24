6e073c65ccaad01a7b8a40755f1f02e3dd6c58fa
Den <New@KOMPUTER.com>
2025-03-24 22:13:28 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,24 +1,37 @@
 ##%%
-from time import sleep
-import os
-import re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+age = 22
+member = True
+deposit = 15
 
-URL = "http://parsinger.ru/selenium/9/9.3.1/index.html"
-browser = webdriver.Chrome()
-browser.get(URL)
-# iframe = browser.find_element('tag name', 'iframe')
-# browser.switch_to.frame(iframe)
-# text = browser.page_source
-# words = ''.join(re.findall(r'\*(.*?)\*', text))
-# print(words)
-sleep(200)
-total_likes = 0
+if age > 18:
+    if member:
+        if deposit > 0:
+            if deposit >= 75:
+                print("Цена билета 0")
+                print('Сняты средства с депозита в размере 75р')
+                deposit -= 75
+            else:
+                print(f"Цена билета {75 - deposit}")
+                print('Ваш депозит полностью потрачен')
+                deposit = 0
+        else:
+            print("Цена билета 75")
+    else:
+        print("Цена билета 100")
+else:
+    if member:
+        if deposit > 0:
+            if deposit >= 25:
+                print("Цена билета 0")
+                print('Сняты средства с депозита в размере 25р')
+                deposit -= 75
+            else:
+                print(f"Цена билета {25 - deposit}")
+                print('Ваш депозит полностью потрачен')
+                deposit = 0
+        else:
+            print("Цена билета 25")
+    else:
+        print("Цена билета 50")
 
 
-print(total_likes)
-
-browser.quit()
-os.system("taskkill /im chrome.exe /f")
