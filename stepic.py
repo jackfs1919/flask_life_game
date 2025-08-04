d9b853d82d7c3038f786be0e3aeffcb712a1579d
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2025-08-04 11:55:12 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,19 +1,7 @@
-import json
-import os
-file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data_workers.json')
-with open(file_path, 'r') as file:
-    workers = json.load(file)
-x = 'firefighter'
-blank = dict.fromkeys(('20-29', '30-39', '40-49', '50-59', '60-69', 'FEMALE', 'MALE'), 0)
+import arrow
 
-print('---------------------')
-print(f'| 20-29  | {blank['20-29']}')
-print(f'| 30-39  | {blank['30-39']}')
-print(f'| 40-49  | {blank['40-49']}')
-print(f'| 50-59  | {blank['60-69']}')
-print(f'| 60-69  | {blank['FEMALE']}')
-print('---------------------')
-print(f'| FEMALE | {blank['MALE']}')
-print(f'| MALE   | {blank['MALE']}')
-print('---------------------')
-print(blank)
+d = arrow.now()
+year = d.format('YYYY')
+START_DATE = str(d.shift(months=-1).replace(day=21).strftime('%d.%m.%Y'))
+END_DATE = str(d.replace(day=20).strftime('%d.%m.%Y'))
+print(START_DATE, END_DATE, type(START_DATE), type(END_DATE))
