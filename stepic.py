b63165cbac962f773e07fadfd7ba6ca82194de03
Den <New@KOMPUTER.com>
2025-09-16 20:17:46 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,7 +1,10 @@
 #№%%
 import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
+log_format = "%(asctime)s--%(levelname)s--%(filename)s, " \
+"line %(lineno)d, %(message)s"
+logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+logging.basicConfig(level='DEBUG', format=log_format)
+# logger.addHandler(logging.StreamHandler())
 logger.info("info")
