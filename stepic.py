b2d4de3a65edf18360d4384f86da785f0d2a67b8
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2025-09-17 10:48:32 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,7 +1,9 @@
 #№%%
 import logging
+import sys
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 logger.info("info")
+logger.info(sys.path)
