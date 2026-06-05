360e0062c59f709917f4b837d2de9515f0799d4e
Den <New@KOMPUTER.com>
2026-06-05 19:49:40 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,29 +1,15 @@
 import asyncio
 
-preparing_count, max_preparing, working_count, max_working = 0, 0, 0, 0
-lock = asyncio.Lock()
-
-async def worker(worker_id, semaphore):
-    global preparing_count, max_preparing, working_count, max_working, lock
-    # Подготовка
-    async with lock:
-        preparing_count += 1
-        max_preparing = max(max_preparing, preparing_count)
-    await asyncio.sleep(0.1)
-    async with semaphore:
-        # Работа
-        async with lock:
-            working_count += 1
-            max_working = max(max_working, working_count)
-        await asyncio.sleep(0.1)
+async def inner():
+    await asyncio.sleep(10)
+    
+async def middle():
+    await asyncio.wait_for(inner(), timeout=2.0)
 
 async def main():
-    semaphore = asyncio.Semaphore(2)
-    tasks = [worker(i, semaphore) for i in range(4)]
-    await asyncio.gather(*tasks)
-    print(max_preparing)
-    print(max_working)
-
-
+    try:
+        await asyncio.wait_for(middle(), timeout=1.0)
+    except asyncio.TimeoutError:
+        print("Сработал внешний тайм-аут")
 if __name__ == "__main__":
     asyncio.run(main())
