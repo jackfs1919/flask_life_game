e3a37368b5c14d794788a44e00ffc01a7c1a6d73
Den <New@KOMPUTER.com>
2026-06-07 09:59:12 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,15 +1,2 @@
-import asyncio
+print((1 + 2) * ("7" + "8"))
 
-async def inner():
-    await asyncio.sleep(10)
-    
-async def middle():
-    await asyncio.wait_for(inner(), timeout=2.0)
-
-async def main():
-    try:
-        await asyncio.wait_for(middle(), timeout=1.0)
-    except asyncio.TimeoutError:
-        print("Сработал внешний тайм-аут")
-if __name__ == "__main__":
-    asyncio.run(main())
