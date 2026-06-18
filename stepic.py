5e09bf01391544631abcdcad388bc1cf1d31a667
Den <New@KOMPUTER.com>
2026-06-18 19:07:23 +0300

    +

--- stepic.py
+++ stepic.py
@@ -1,2 +1,7 @@
-*lst, x, y, z = input().split()
-print(*lst, x, y, z)
+# s = input().strip()
+s = ['A', 'O', 'O', 'O', 'Z', 'A', 'O', 'Z']
+a = [i for i, c in enumerate(s) if c == 'A']
+z = [i for i, c in enumerate(s) if c == 'Z']
+distances = [abs(i - j) for i in a for j in z]
+print(min(distances) if distances else -1)
+print(a, z)
